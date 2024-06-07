636866a4852aaea9e4482a774624472e1984250f
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-08 00:20:03 +0530

    Update bot.py

--- DAXXMUSIC/core/bot.py
+++ DAXXMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class DAXX(Client):
     def __init__(self, name):
         LOGGER(name).info(f"Starting Bot...")
         super().__init__(
-            name=DAXXMUSIC,
+            name="DAXXMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
