75b9aa8d03361ca6252c06d899b8686f720602e1
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-06-08 22:07:23 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -15,7 +15,9 @@ heroku()
 app = DAXX()
 api = SafoneAPI()
 userbot = Userbot()
-
+user = Client("daxx",
+              api_id="16874790",
+              api_hash="46aa49adca0f1d184eb2a2f4a48a1df9")
 
 from .platforms import *
 
