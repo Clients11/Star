a952c2bbe8570a997961c430ff0621053bb061a0
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-06-08 22:15:45 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -3,7 +3,7 @@ from DAXXMUSIC.core.dir import dirr
 from DAXXMUSIC.core.git import git
 from DAXXMUSIC.core.userbot import Userbot
 from DAXXMUSIC.misc import dbb, heroku
-
+from pyrogram import Client
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
