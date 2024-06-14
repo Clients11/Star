2cbb31df6c7cbf054817dc81c67cca107e0c99da
DREAMERS-BOTS <159102147+DREAMERS-BOTS@users.noreply.github.com>
2024-06-14 12:26:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DREAMER-BOTS/DAXXMUSIC",
+    "https://github.com/DREAMERS-BOTS/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
