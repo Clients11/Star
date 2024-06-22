c6f3f94e492f4bdc85bc718915fcd7dc9fec6fb8
DREAMERS-BOTS <159102147+DREAMERS-BOTS@users.noreply.github.com>
2024-06-22 23:31:06 +0530

    Update scr.py

--- DAXXMUSIC/plugins/tools/scr.py
+++ DAXXMUSIC/plugins/tools/scr.py
@@ -73,7 +73,7 @@ async def cmd_scr(client, message):
             if not cards:
                 continue
             
-            file_name = f"{limit}x_CC_Scraped_By_@YesikooBot.txt"
+            file_name = f"{limit}x_CC_Scraped_By_@daisy_12_bot.txt"
             for item in cards:
                 amt_cc += 1
                 cc, mes, ano, cvv = item
