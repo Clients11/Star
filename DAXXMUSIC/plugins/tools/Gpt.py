64064d5563bb46bc706b8c27907a8a559b2d1b38
Starboymusic <ooaooaoa>
2024-09-01 00:14:16 +0530

    update

--- DAXXMUSIC/plugins/tools/Gpt.py
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @DAISY_12_BOT",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @Shinhari_Musicbot",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
