7b4c21114dd38df4b35032f2c16d2cb863d39001
Starboymusic <alxkoaks>
2024-09-01 00:32:10 +0530

    update whisper.py

--- DAXXMUSIC/plugins/tools/whisper.py
+++ DAXXMUSIC/plugins/tools/whisper.py
@@ -111,7 +111,7 @@ async def in_help():
         InlineQueryResultArticle(
             title="💒 Whisper",
             description=f"@Shinhari_Musicbot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Shinhari_Musicbot (Target Username or ID) (Your Message).\n\n**Example:**\n@Shinhari_Musicbot @username I Wanna Phuck You"),
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Shinhari_Musicbot (Target Username or ID) (Your Message).\n\n**Example:**\n@Shinhari_Musicbot @usernameI Love You 💕"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
